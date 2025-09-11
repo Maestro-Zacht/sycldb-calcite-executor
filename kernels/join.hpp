f9db5cdcf00a0004dc717aabc22f7fb29da02544
Matteo Ghia <61846068+Maestro-Zacht@users.noreply.github.com>
2025-09-11 09:59:00 +0200

    converted join to sycl

--- kernels/join.hpp
+++ kernels/join.hpp
@@ -20,31 +20,21 @@ void build_keys_ht(T col[], bool flags[], int col_len, bool ht[], T ht_len, T ht
 
 void build_key_vals_ht(int col[], int agg_col[], bool flags[], int col_len, int ht[], int ht_len, int ht_min_value, sycl::queue &queue)
 {
-    for (int i = 0; i < col_len; i++)
-    {
-        if (flags[i])
-        {
-            int hash = HASH(col[i], ht_len, ht_min_value);
-            ht[hash << 1] = 1;
-            ht[(hash << 1) + 1] = agg_col[i];
-        }
-        else
-            ht[HASH(col[i], ht_len, ht_min_value) << 1] = 0;
-    }
-    // queue.parallel_for(
-    //          col_len,
-    //          [=](sycl::id<1> i)
-    //          {
-    //              if (flags[i])
-    //              {
-    //                  int hash = HASH(col[i], ht_len, ht_min_value);
-    //                  ht[hash << 1] = 1;
-    //                  ht[(hash << 1) + 1] = agg_col[i];
-    //              }
-    //              else
-    //                  ht[HASH(col[i], ht_len, ht_min_value) << 1] = 0;
-    //          })
-    //     .wait();
+    queue.parallel_for(
+             sycl::range<1>{(unsigned long)col_len},
+             [=](sycl::id<1> idx)
+             {
+                 auto i = idx[0];
+                 if (flags[i])
+                 {
+                     int hash = HASH(col[i], ht_len, ht_min_value);
+                     ht[hash << 1] = 1;
+                     ht[(hash << 1) + 1] = agg_col[i];
+                 }
+                 else
+                     ht[HASH(col[i], ht_len, ht_min_value) << 1] = 0;
+             })
+        .wait();
 }
 
 template <typename T>
@@ -58,34 +48,36 @@ void filter_join(T build_col[],
                  int probe_col_len, sycl::queue &queue)
 {
     int ht_len = build_max_value - build_min_value + 1;
-    // bool *ht = new bool[ht_len];
-    // std::fill_n(ht, ht_len, false);
     bool *ht = sycl::malloc_shared<bool>(ht_len, queue);
     queue.fill(ht, false, ht_len).wait();
 
     build_keys_ht(build_col, build_flags, build_col_len, ht, ht_len, build_min_value, queue);
 
-    for (int i = 0; i < probe_col_len; i++)
-        if (probe_col_flags[i])
-            probe_col_flags[i] = ht[HASH(probe_col[i], ht_len, build_min_value)];
+    queue.parallel_for(
+             sycl::range<1>{(unsigned long)probe_col_len},
+             [=](sycl::id<1> idx)
+             {
+                 auto i = idx[0];
+                 if (probe_col_flags[i])
+                     probe_col_flags[i] = ht[HASH(probe_col[i], ht_len, build_min_value)];
+             })
+        .wait();
 
-    // delete[] ht;
     sycl::free(ht, queue);
 }
 
-// TODO: move to GPU
 void full_join(TableData<int> &probe_table,
                TableData<int> &build_table,
                int probe_col_index,
                int build_col_index, sycl::queue &queue)
 {
-    int ht_len = build_table.columns[build_table.column_indices.at(build_col_index)].max_value -
-                 build_table.columns[build_table.column_indices.at(build_col_index)].min_value + 1;
-    int *ht = new int[ht_len * 2],
-        build_column = build_table.column_indices.at(build_col_index),
-        probe_column = probe_table.column_indices.at(probe_col_index);
+    int build_column = build_table.column_indices.at(build_col_index),
+        probe_column = probe_table.column_indices.at(probe_col_index),
+        ht_len = build_table.columns[build_column].max_value -
+                 build_table.columns[build_column].min_value + 1,
+        *ht = sycl::malloc_shared<int>(ht_len * 2, queue);
 
-    std::fill_n(ht, ht_len * 2, 0); // maybe it's not necessary to initialize
+    queue.fill(ht, 0, ht_len * 2).wait();
 
     build_key_vals_ht(
         build_table.columns[build_column].content,
@@ -93,18 +85,30 @@ void full_join(TableData<int> &probe_table,
         build_table.flags, build_table.col_len, ht, ht_len,
         build_table.columns[build_column].min_value, queue);
 
-    for (int i = 0; i < probe_table.col_len; i++)
-    {
-        if (probe_table.flags[i])
-        {
-            int hash = HASH(probe_table.columns[probe_column].content[i], ht_len,
-                            build_table.columns[build_column].min_value);
-            if (ht[hash << 1] == 1)
-                probe_table.columns[probe_column].content[i] = ht[(hash << 1) + 1]; // replace the probe column value with the value to group by on
-            else
-                probe_table.flags[i] = false; // mark as not selected
-        }
-    }
+    bool *probe_flags = probe_table.flags;
+    int *probe_content = probe_table.columns[probe_column].content,
+        build_col_min = build_table.columns[build_column].min_value,
+        build_col_max = build_table.columns[build_column].max_value;
+
+    queue.parallel_for(
+             sycl::range<1>{(unsigned long)probe_table.col_len},
+             [=](sycl::id<1> idx)
+             {
+                 auto i = idx[0];
+                 if (probe_flags[i])
+                 {
+                     int hash = HASH(probe_content[i], ht_len,
+                                     build_col_min);
+                     if (probe_content[i] >= build_col_min &&
+                         probe_content[i] <= build_col_max &&
+                         ht[hash << 1] == 1)
+                         probe_content[i] = ht[(hash << 1) + 1]; // replace the probe column value with the value to group by on
+                     else
+                         probe_flags[i] = false; // mark as not selected
+                 }
+             })
+        .wait();
+
     // the group by column index must refer to the old foreign key
     probe_table.column_indices.erase(probe_col_index);
     probe_table.column_indices[probe_table.col_number + build_table.group_by_column] = probe_column;
@@ -113,5 +117,5 @@ void full_join(TableData<int> &probe_table,
     probe_table.columns[probe_column].min_value = build_table.columns[build_table.group_by_column].min_value;
     probe_table.columns[probe_column].max_value = build_table.columns[build_table.group_by_column].max_value;
 
-    delete[] ht;
+    sycl::free(ht, queue);
 }
